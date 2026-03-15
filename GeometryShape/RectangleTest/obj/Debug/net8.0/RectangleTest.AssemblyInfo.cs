96564432954004bdd8856375fecf8e80c702cafe
ilonaSag <kupoles09@gmail.com>
2026-03-15 21:49:54 +0500

    trianglecorrect5

--- GeometryShape/RectangleTest/obj/Debug/net8.0/RectangleTest.AssemblyInfo.cs
+++ GeometryShape/RectangleTest/obj/Debug/net8.0/RectangleTest.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("GeometryShape")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("RectangleTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b43d20c2d0d00a3633933f812c20dc3f50595551")]
-[assembly: System.Reflection.AssemblyProductAttribute("GeometryShape")]
-[assembly: System.Reflection.AssemblyTitleAttribute("GeometryShape")]
+[assembly: System.Reflection.AssemblyProductAttribute("RectangleTest")]
+[assembly: System.Reflection.AssemblyTitleAttribute("RectangleTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
