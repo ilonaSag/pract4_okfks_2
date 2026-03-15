c4529bce3f34e434d4b2722b8edbcfc937db1c1d
ilonaSag <kupoles09@gmail.com>
2026-03-15 23:37:54 +0500

    Complete cleanup and recreate tests

---
+++ GeometryShape.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Xunit; 
+using GeometryShape; 
+ 
+namespace GeometryShape.Tests 
+{ 
+    public class UnitTest1 
+    { 
+        [Fact] 
+        public void Test1() 
+        { 
+            Assert.True(true); 
+        } 
+    } 
+} 
